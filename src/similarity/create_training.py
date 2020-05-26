86d0a339f2a23ab02547ffd2005e579a8aeaf480
Daniel Obraczka <obraczka@informatik.uni-leipzig.de>
2020-05-26 19:20:37 +0200

    Enabled create_training to create 3 seperate dfs

--- src/similarity/create_training.py
+++ src/similarity/create_training.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import yaml
 import sys
+import os
 from typing import List
 from openea.modules.load.kgs import KGs, read_kgs_from_folder
 from similarity.similarities import (
@@ -57,69 +58,19 @@ def create_labeled_similarity_frame(
     :param labeled_tuples: list of triples with the first two entries denoting the entity tuples and the last the label
     :return: SparseDataFrame with labels if available
     """
-    similarities_new = dict()
-    for k, v in similarities.items():
-        similarities_new[_create_tuple_string(k)] = v
-    lablist = []
+    tmp_lab_dict = dict()
     for t in labeled_tuples:
-        lablist.append([_create_tuple_string(t), t[2]])
-    # create label frame
-    lab_frame = pd.DataFrame(lablist, columns=["ids", "label"])
-    lab_frame.set_index("ids", inplace=True)
-    # create similarity frame
+        tmp_lab_dict[(t[0], t[1])] = t[2]
     sim_frame = pd.SparseDataFrame.from_dict(
-        similarities_new, orient="index", dtype="float32"
+        similarities, orient="index", dtype="float32"
     )
+    lab_frame = pd.Series(tmp_lab_dict).to_frame("label")
     sim_frame = sim_frame.merge(
         lab_frame, left_index=True, right_index=True, how="outer"
     )
     return create_normalized_sim_from_dist_cols(
         sim_frame, _get_columns_to_normalize(sim_frame, cols_to_normalize)
     )
-
-
-# def create_from_similarities(similarities: dict, labeled_tuples: List):
-#     """
-#     :param similarities: dictionary of dictionaries of similarities per entity tuple
-#     :param labeled_tuples: list of triples with the first two entries denoting the entity tuples and the last the label
-#     :return: labeled features as sparse matrix, labels of features as numpy array,
-#         names of the features (which also describes the columns of the sparse feature matrices), unlabeled features as sparse matrix
-#     """
-#     sim_frame = create_labeled_sim_frame(similarities, labeled_tuples)
-#     features_frame = sim_frame[sim_frame.label.notna()]
-#     features_unlabeled_frame = sim_frame[sim_frame.label.isna()]
-#     # create sparse dateframe with tuples as columns
-#     sim_frame = pd.SparseDataFrame(similarities)
-#     # get positive and negative labels
-#     positive_tuples = []
-#     negative_tuples = []
-#     labels = []
-#     for l in labeled_tuples:
-#         if l[2] == 1:
-#             positive_tuples.append((l[0], l[1]))
-#         else:
-#             negative_tuples.append((l[0], l[1]))
-#         labels.append(l[2])
-
-#     # remove positive labeled
-#     unlabeled = sim_frame.drop(positive_tuples, axis="columns")
-#     # create sparse matrix where each row contains similarities of entity tuple
-#     sparse_positive = csr_matrix(sim_frame[positive_tuples].T)
-#     if len(negative_tuples) > 0:
-#         sparse_negative = csr_matrix(sim_frame[negative_tuples].T)
-#         features_unlabeled = sim_frame.drop(negative_tuples, axis="columns").T
-#         # append positive and negative
-#         features = vstack((sparse_positive, sparse_negative), format="csr")
-#     else:
-#         features_unlabeled = csr_matrix(unlabeled.T)
-#         features = sparse_positive
-#     feature_names = sim_frame.index.values.tolist()
-#     return (
-#         features,
-#         np.array(labels),
-#         feature_names,
-#         features_unlabeled,
-#     )
 
 
 def read_entity_ids(path: str) -> dict:
@@ -161,7 +112,7 @@ def create_feature_similarity_frame(
     similarities = calculate_from_embeddings_with_training(
         embedding, labeled_tuples, kgs, metric
     )
-    print("Finished calculation from training")
+    print("Finished calculation from given links")
     if not only_training:
         similarities_embedding = calculate_from_embeddings(
             embedding, kgs, n_neighbors, metric
@@ -177,6 +128,8 @@ if __name__ == "__main__":
         arguments = yaml.load(f, yaml.FullLoader)
 
     output = arguments["output"]
+    if not os.path.exists(output):
+        os.makedirs(output)
 
     # Load all necessary files
     embedding = np.load(arguments["embedding"])
@@ -192,30 +145,31 @@ if __name__ == "__main__":
         arguments["ordered"],
         remove_unlinked=remove_unlinked,
     )
-
-    wanted_links = (
-        arguments["wanted_links"] if "wanted_links" in arguments else "train_links"
-    )
-    labeled_tuples = read_examples(
-        training_data_folder + division + wanted_links, kg1_entities, kg2_entities,
-    )
-
-    if "sample_negative" in arguments and arguments["sample_negative"]:
-        labeled_tuples.extend(sample_negative(labeled_tuples))
-
+    # dont use nearest neighbors if true
     only_training = (
         arguments["only_training"] if "only_training" in arguments else False
     )
     metric = arguments["metric"] if "metric" in arguments else "euclidean"
+    link_names = ["test_links", "train_links", "valid_links"]
 
-    # feature vector creation
-    features_frame = create_feature_similarity_frame(
-        embedding,
-        labeled_tuples,
-        kgs,
-        arguments["nearest_neighbors"],
-        metric,
-        only_training,
-    )
-    features_frame.to_pickle(output, protocol=2)
-    print(f"Wrote similarity frame to {output}")
+    for wanted_links in link_names:
+        print(f"Prepare creation of similarities for {wanted_links}")
+        labeled_tuples = read_examples(
+            training_data_folder + division + wanted_links, kg1_entities, kg2_entities,
+        )
+
+        if "sample_negative" in arguments and arguments["sample_negative"]:
+            labeled_tuples.extend(sample_negative(labeled_tuples))
+
+        # feature vector creation
+        features_frame = create_feature_similarity_frame(
+            embedding,
+            labeled_tuples,
+            kgs,
+            arguments["nearest_neighbors"],
+            metric,
+            only_training,
+        )
+        out_file_path = output + "/" + wanted_links + ".pkl"
+        features_frame.to_pickle(out_file_path, protocol=2)
+        print(f"Wrote similarity frame for {wanted_links} to {out_file_path}")
